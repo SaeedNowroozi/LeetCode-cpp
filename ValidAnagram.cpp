f5b48115064f5c5cf6e15f9237a328d422234eff
SaeedNowroozi <38817647+SaeedNowroozi@users.noreply.github.com>
2022-12-25 21:39:17 +0330

    Update ValidAnagram.cpp

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -4,3 +4,13 @@
         if (s.compare(t) == 0) return true;
         return false;
     }
+//////////////////////
+ bool isAnagram(string s, string t) {
+ unordered_map<char,int> mp;
+         for(auto i:s) mp[i]++;
+         for(auto i:t) mp[i]--;
+         for(auto i = mp.begin(); i != mp.end();i++){
+        if(i->second != 0)
+        return false;}
+        return true;
+ }
